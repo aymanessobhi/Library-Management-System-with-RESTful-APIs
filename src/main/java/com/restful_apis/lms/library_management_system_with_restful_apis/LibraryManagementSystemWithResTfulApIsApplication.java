9ca89fd78f7839965a0903d915354129be369bf2
aymanessobhi <ayman.essobhi@gmail.com>
2023-07-13 15:36:17 +0100

    first commit

---
+++ src/main/java/com/restful_apis/lms/library_management_system_with_restful_apis/LibraryManagementSystemWithResTfulApIsApplication.java
@@ -0,0 +1,13 @@
+package com.restful_apis.lms.library_management_system_with_restful_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemWithResTfulApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemWithResTfulApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restful_apis/lms/library_management_system_with_restful_apis/LibraryManagementSystemWithResTfulApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_apis.lms.library_management_system_with_restful_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemWithResTfulApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
